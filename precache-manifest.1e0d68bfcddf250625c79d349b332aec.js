3524709603fac43afbdcde5b418ed2cb216f4a5a
olenazubashenko <olenazubashenko@gmail.com>
2019-11-28 14:58:44 +0200

    Updates

---
+++ precache-manifest.1e0d68bfcddf250625c79d349b332aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6dc72ab9dc8282debc80130dafe34ca",
+    "url": "/ReactMortalCombat/index.html"
+  },
+  {
+    "revision": "18e85bed29d92a6aec95",
+    "url": "/ReactMortalCombat/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f55ac8c2ea8403734db6",
+    "url": "/ReactMortalCombat/static/js/2.0b644bdd.chunk.js"
+  },
+  {
+    "revision": "18e85bed29d92a6aec95",
+    "url": "/ReactMortalCombat/static/js/main.81af7b46.chunk.js"
+  },
+  {
+    "revision": "3e4a45a0caf5240838ea",
+    "url": "/ReactMortalCombat/static/js/runtime-main.72aff12b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ReactMortalCombat/static/media/logo.25bf045c.svg"
+  }
+]);
