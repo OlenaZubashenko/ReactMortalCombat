9dd6a48a937341a39b0638e4b1c8cd5b2a74f89e
olenazubashenko <olenazubashenko@gmail.com>
2019-12-09 23:09:41 +0200

    Updates

---
+++ precache-manifest.dd8c29f14c32589a8b1366ea2fb5c8f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185ffb436c4fd7cb410a211aa1040e9e",
+    "url": "/ReactMortalCombat/index.html"
+  },
+  {
+    "revision": "682bd29c37f248b34347",
+    "url": "/ReactMortalCombat/static/css/main.8d04cba8.chunk.css"
+  },
+  {
+    "revision": "bd621f639b4110121f57",
+    "url": "/ReactMortalCombat/static/js/2.02177a05.chunk.js"
+  },
+  {
+    "revision": "682bd29c37f248b34347",
+    "url": "/ReactMortalCombat/static/js/main.247e1b7f.chunk.js"
+  },
+  {
+    "revision": "3e4a45a0caf5240838ea",
+    "url": "/ReactMortalCombat/static/js/runtime-main.72aff12b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a20f65ada9f27682ce288c90351aa4ea.js"
+  "/ReactMortalCombat/precache-manifest.dd8c29f14c32589a8b1366ea2fb5c8f5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactMortalCombat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
