d03d47d333a3aa4835672c99531e8210a897adf8
olenazubashenko <olenazubashenko@gmail.com>
2019-12-09 16:49:20 +0200

    refactoring

--- src/components/Battle/battle.js
+++ src/components/Battle/battle.js
@@ -3,54 +3,52 @@ import './battle.css'
 import HeroList from '../HeroList/hero-list';
 import BattleIcons from '../BattleIcons/battle-icons'
 
-export default class Battle  extends React.Component { 
+export default class Battle extends React.Component {
     constructor(props) {
         super(props);
-        this.state=props.location.state;
+        this.state = props.location.state;
     }
-    render () {
-const 
+    render() {
+        const
 
-        battleImagesArr = [
-            {key:'1',img : '/img/circle-dragon.png'},
-            {key:'2',img : '/img/circle-dragon.png'},
-            {key:'3',img : '/img/circle-dragon.png'},
-            {key:'4',img : '/img/circle-dragon.png'},
-            {key:'5',img : '/img/circle-dragon.png'},
-            {key:'6',img : '/img/circle-dragon.png'},
-            {key:'7',img : '/img/circle-dragon.png'}
-            
-        ]
+            battleImagesArr = [
+                { key: '1', img: '/img/circle-dragon.png' },
+                { key: '2', img: '/img/circle-dragon.png' },
+                { key: '3', img: '/img/circle-dragon.png' },
+                { key: '4', img: '/img/circle-dragon.png' },
+                { key: '5', img: '/img/circle-dragon.png' },
+                { key: '6', img: '/img/circle-dragon.png' }                
+
+            ]
         const elements = battleImagesArr.map((item) => {
             return (
-    <ul className='image-item-list'>
-            <div><img src={this.state.selectedHero.url}/></div>
-    <BattleIcons 
-                    {...item}
+                <ul className='image-item-list'>
+                    <BattleIcons
+                        {...item}
                     />
-    </ul>
-                             
+                </ul>
+
             )
         })
         //const { selected } = this.props.selected;
         return (
-            
-                <div className='battle-wrapper'
+
+            <div className='battle-wrapper'
             >
                 <h1>Battle 1</h1>
-                <ul>
-        <li>From Battle Component</li>
-                    <li>From Battle Component</li>
+                <ul className='heroes-battle-wrapper'>
+                    <div><img src={this.state.selectedHero.url}/></div>
+                    <div><img src={this.state.selectedHero.url}/></div>
                 </ul>
                 <div className='icons-wrapper'>
-                {elements}
+                    {elements}
                 </div>
-              
+
             </div>
-    
+
         )
     }
-    
+
 };
 
 
